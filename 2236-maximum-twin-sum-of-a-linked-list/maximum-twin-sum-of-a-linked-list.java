fc9c488999d5dc2a569ab09bfb00058464cba2ca
Ankush_Gaur <90082938+ankush-gaur4@users.noreply.github.com>
2026-06-14 16:56:00 +0530

    Time: 29 ms (5.16%) | Memory: 120.4 MB (5.07%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -8,26 +8,49 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- 
+
  //Approach-1 (Using vector/array)
 
 class Solution {
     public int pairSum(ListNode head) {
-        List<Integer> vec = new ArrayList<>();
+        // List<Integer> vec = new ArrayList<>();
 
-        while (head != null){
-            vec.add(head.val);
-            head = head.next;
+        // while (head != null){
+        //     vec.add(head.val);
+        //     head = head.next;
+        // }
+        // int i = 0;
+        // int j = vec.size()- 1;
+        // int result = 0;
+
+        // while(i < j) {
+        //     result = Math.max(result, vec.get(i)+ vec.get(j));
+        //     i++;
+        //     j--;
+        // }
+        // return result;
+
+//Approach-2 (Using Stack)
+        Stack<Integer> st = new Stack<>();
+        ListNode curr = head;
+
+        while (curr != null) {
+            st.push(curr.val);
+            curr = curr.next;
         }
-        int i = 0;
-        int j = vec.size()- 1;
+
+        curr = head;
+        int count = 1;
+        int n = st.size();
         int result = 0;
 
-        while(i < j) {
-            result = Math.max(result, vec.get(i)+ vec.get(j));
-            i++;
-            j--;
+        while (count <= n / 2) {
+            result = Math.max(result, curr.val + st.peek());
+            curr = curr.next;
+            st.pop();
+            count++;
         }
+
         return result;
     }
 }
